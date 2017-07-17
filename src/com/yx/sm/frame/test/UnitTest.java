4e31642304c7fcdce0c18925c5449a7a12edc2ac
Administrator <Administrator@AFAAW-705280141>
2017-07-17 15:28:59 +0800

    111

--- src/com/yx/sm/frame/test/UnitTest.java
+++ src/com/yx/sm/frame/test/UnitTest.java
@@ -1,5 +1,8 @@
 package com.yx.sm.frame.test;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.junit.Test;
 
 public class UnitTest {
@@ -9,6 +12,34 @@ public class UnitTest {
 		int[] a = new int[1];
 		a[0] = 2;
 		System.out.println(a[0]);
+	}
+	
+	@Test
+	public void test2() {
+		Map<String , Integer> map = new TreeMap<String , Integer>();   
+		map.put("B" , 2);
+		map.put("A" , 1);
+		map.put("C" , 3);
+		map.put("D" , 4);
+		map.put("E" , 5);   
+		map.put("F" , 6);
+		map.put("G" , 7);
+		map.put("H" , 8);   
+		map.put("I" , 9);
+		map.put("J" , 10);
+		
+		for (String v:map.keySet()) {
+			System.out.print(v+"  ");
+		}
+		System.out.println(" ---------------------");
+		for (Integer v:map.values()) {
+			System.out.print(v+"  ");
+		}
+		System.out.println(" ---------------------");
+		for (int i = map.size()-1; i >= 0; i--) {
+			Object[] str = map.keySet().toArray();
+			System.out.print(map.get(str[i])+"  ");
+		}
 	}
 
 }
